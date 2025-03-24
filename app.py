0c2db062ed94875645e0b62d988b17b675c01b4e
Akbar.Gholami <ak1.g@outlook.com>
2025-03-24 14:17:21 +0100

    Added a new route

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def ping():
     return "server is working123"
 
+@app.route("/home")
+def home():
+   return "Welcome to the home page"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
